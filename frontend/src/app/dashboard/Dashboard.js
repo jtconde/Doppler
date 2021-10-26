0e48864ac3808c3e48d908cf279cccfc8a953fe2
jtconde <juliantcinde@gmail.com>
2021-10-26 07:17:36 -0400

    Update styling and layout

--- frontend/src/app/dashboard/Dashboard.js
+++ frontend/src/app/dashboard/Dashboard.js
@@ -56,25 +56,13 @@ export class Dashboard extends Component {
   render () {
     return (
       <div>
+        <div>
+          <h3>Amount Invested</h3>
+        </div>
         <div className="row">
-          <div className="col-12 grid-margin stretch-card">
-            <div className="card corona-gradient-card">
-              <div className="card-body py-0 px-0 px-sm-3">
-                <div className="row align-items-center">
-                  <div className="col-4 col-sm-3 col-xl-2">
-                    <img src={require('../../assets/images/dashboard/Group126@2x.png')} className="gradient-corona-img img-fluid" alt="banner" />
-                  </div>
-                  <div className="col-5 col-sm-7 col-xl-8 p-0">
-                    <h4 className="mb-1 mb-sm-0">New refreshing look</h4>
-                    <p className="mb-0 font-weight-normal d-none d-sm-block">Corona admin template now with a new facelift for enhanced legibility and aesthetics!</p>
-                  </div>
-                  <div className="col-3 col-sm-2 col-xl-2 pl-0 text-center">
-                    <button className="btn btn-outline-light btn-rounded get-started-btn">Get Started</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <h2>$50,000</h2>
+          <p className="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
+
         </div>
         <div className="row">
           <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
--- frontend/src/app/shared/Navbar.js
+++ frontend/src/app/shared/Navbar.js
@@ -23,110 +23,15 @@ class Navbar extends Component {
           <ul className="navbar-nav w-100">
             <li className="nav-item w-100">
               <form className="nav-link mt-2 mt-md-0 d-none d-lg-flex search">
-                <input type="text" className="form-control" placeholder="Search products" />
+                
+                <input type="text" className="form-control" placeholder="Search Crypto" />
               </form>
             </li>
           </ul>
           <ul className="navbar-nav navbar-nav-right">
-            <Dropdown alignRight as="li" className="nav-item d-none d-lg-block">
-                <Dropdown.Toggle className="nav-link btn btn-success create-new-button no-caret">
-                + <Trans>Create New Project</Trans>
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu className="navbar-dropdown preview-list create-new-dropdown-menu">
-                  <h6 className="p-3 mb-0"><Trans>Projects</Trans></h6>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="!#" onClick={evt =>evt.preventDefault()} className="preview-item">
-                    <div className="preview-thumbnail">
-                      <div className="preview-icon bg-dark rounded-circle">
-                        <i className="mdi mdi-file-outline text-primary"></i>
-                      </div>
-                    </div>
-                    <div className="preview-item-content">
-                      <p className="preview-subject ellipsis mb-1"><Trans>Software Development</Trans></p>
-                    </div>
-                  </Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="!#" onClick={evt =>evt.preventDefault()} className="preview-item">
-                    <div className="preview-thumbnail">
-                      <div className="preview-icon bg-dark rounded-circle">
-                        <i className="mdi mdi-web text-info"></i>
-                      </div>
-                    </div>
-                    <div className="preview-item-content">
-                      <p className="preview-subject ellipsis mb-1"><Trans>UI Development</Trans></p>
-                    </div>
-                  </Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="!#" onClick={evt =>evt.preventDefault()} className="preview-item">
-                    <div className="preview-thumbnail">
-                      <div className="preview-icon bg-dark rounded-circle">
-                        <i className="mdi mdi-layers text-danger"></i>
-                      </div>
-                    </div>
-                    <div className="preview-item-content">
-                      <p className="preview-subject ellipsis mb-1"><Trans>Software Testing</Trans></p>
-                    </div>
-                  </Dropdown.Item>
-                  <Dropdown.Divider />
-                  <p className="p-3 mb-0 text-center"><Trans>See all projects</Trans></p>
-                </Dropdown.Menu>
-              </Dropdown>
-            <li className="nav-item d-none d-lg-block">
-              <a className="nav-link" href="!#" onClick={event => event.preventDefault()}>
-                <i className="mdi mdi-view-grid"></i>
-              </a>
-            </li>
-            <Dropdown alignRight as="li" className="nav-item border-left" >
-              <Dropdown.Toggle as="a" className="nav-link count-indicator cursor-pointer">
-                <i className="mdi mdi-email"></i>
-                <span className="count bg-success"></span>
-              </Dropdown.Toggle>
-              <Dropdown.Menu className="navbar-dropdown preview-list">
-                  <h6 className="p-3 mb-0"><Trans>Messages</Trans></h6>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="!#" onClick={evt =>evt.preventDefault()} className="preview-item">
-                    <div className="preview-thumbnail">
-                      <div className="preview-icon bg-dark rounded-circle">
-                        <img src={require('../../assets/images/faces/face4.jpg')} alt="profile" className="rounded-circle profile-pic" />
-                      </div>
-                    </div>
-                    <div className="preview-item-content">
-                      <p className="preview-subject ellipsis mb-1"><Trans>Mark send you a message</Trans></p>
-                      <p className="text-muted mb-0"> 1 <Trans>Minutes ago</Trans> </p>
-                    </div>
-                  </Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="!#" onClick={evt =>evt.preventDefault()} className="preview-item">
-                    <div className="preview-thumbnail">
-                      <div className="preview-icon bg-dark rounded-circle">
-                        <img src={require('../../assets/images/faces/face2.jpg')} alt="profile" className="rounded-circle profile-pic" />
-                      </div>
-                    </div>
-                    <div className="preview-item-content">
-                      <p className="preview-subject ellipsis mb-1"><Trans>Cregh send you a message</Trans></p>
-                      <p className="text-muted mb-0"> 15 <Trans>Minutes ago</Trans> </p>
-                    </div>
-                  </Dropdown.Item>
-                  <Dropdown.Divider />
-                  <Dropdown.Item href="!#" onClick={evt =>evt.preventDefault()} className="preview-item">
-                    <div className="preview-thumbnail">
-                      <div className="preview-icon bg-dark rounded-circle">
-                        <img src={require('../../assets/images/faces/face3.jpg')} alt="profile" className="rounded-circle profile-pic" />
-                      </div>
-                    </div>
-                    <div className="preview-item-content">
-                      <p className="preview-subject ellipsis mb-1"><Trans>Profile picture updated</Trans></p>
-                      <p className="text-muted mb-0"> 18 <Trans>Minutes ago</Trans> </p>
-                    </div>
-                  </Dropdown.Item>
-                  <Dropdown.Divider />
-                  <p className="p-3 mb-0 text-center">4 <Trans>new messages</Trans></p>
-                </Dropdown.Menu>
-            </Dropdown>
             <Dropdown alignRight as="li" className="nav-item border-left">
               <Dropdown.Toggle as="a" className="nav-link count-indicator cursor-pointer">
-                <i className="mdi mdi-bell"></i>
+                <i className="mdi mdi-bell" ></i>
                 <span className="count bg-danger"></span>
               </Dropdown.Toggle>
               <Dropdown.Menu className="dropdown-menu navbar-dropdown preview-list">
