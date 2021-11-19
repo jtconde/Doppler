2d061b980c1277cde137b92b66bd0a7ae5898052
jtconde <juliantcinde@gmail.com>
2021-11-19 07:22:24 -0500

    updated leaderboard

--- frontend/src/app/dashboard/Dashboard.js
+++ frontend/src/app/dashboard/Dashboard.js
@@ -198,13 +198,13 @@ options = {
          {/*Trending*/}
          
          <div className="portfolio-space">
-         <div className="trend-label">  
+           
+         <div className="trend-title">  
          <p style={{ fontSize: "18px"}}>Trending Last 7 Days</p>
          </div>
-         <div className="see-all">
-         <a href="">See All</a>
-         </div>
-        <div className="portfolio-space">
+         
+         
+        
         <div className="row">
           {/*Start Trending Tile*/}
           <div className="col-xl-3 col-sm-6 grid-margin stretch-card">
@@ -256,210 +256,27 @@ options = {
                 <p className="trend-change text-success">+19.02%</p>
               </div>
               </div>
-            </div>
+           </div>
             {/*End Trending Tile*/}
+            
             </div>
+            
         </div>
-        <div className="row">
-          <div className="col-md-4 grid-margin stretch-card">
-            <div className="card">
-              <div className="card-body">
-                <h4 className="card-title">Transaction History</h4>
-                <div className="aligner-wrapper">
-                  <Doughnut data={this.transactionHistoryData} options={this.transactionHistoryOptions} />
-                  <div className="absolute center-content">
-                    <h5 className="font-weight-normal text-whiite text-center mb-2 text-white">1200</h5>
-                    <p className="text-small text-muted text-center mb-0">Total</p>
-                  </div>
-                </div>  
-                <div className="bg-gray-dark d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
-                  <div className="text-md-center text-xl-left">
-                    <h6 className="mb-1">Transfer to Paypal</h6>
-                    <p className="text-muted mb-0">07 Jan 2019, 09:12AM</p>
-                  </div>
-                  <div className="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
-                    <h6 className="font-weight-bold mb-0">$236</h6>
-                  </div>
-                </div>
-                <div className="bg-gray-dark d-flex d-md-block d-xl-flex flex-row py-3 px-4 px-md-3 px-xl-4 rounded mt-3">
-                  <div className="text-md-center text-xl-left">
-                    <h6 className="mb-1">Tranfer to Stripe</h6>
-                    <p className="text-muted mb-0">07 Jan 2019, 09:12AM</p>
-                  </div>
-                  <div className="align-self-center flex-grow text-right text-md-center text-xl-right py-md-2 py-xl-0">
-                    <h6 className="font-weight-bold mb-0">$593</h6>
-                  </div>
-                </div>
-              </div>
+        <div className="portfolio-space">
+            <div className="see-all">
+         <a href="">See All Trending</a>
+         </div>
             </div>
-          </div>
-          </div>
-          <div className="col-md-8 grid-margin stretch-card">
+            {/*End Trending Section*/}
+        
+          
+       
+        
+        
+          <div className="leaderboard-card portfolio-space">
             <div className="card">
               <div className="card-body">
-                <div className="d-flex flex-row justify-content-between">
-                  <h4 className="card-title mb-1">Open Projects</h4>
-                  <p className="text-muted mb-1">Your data status</p>
-                </div>
-                <div className="row">
-                  <div className="col-12">
-                    <div className="preview-list">
-                      <div className="preview-item border-bottom">
-                        <div className="preview-thumbnail">
-                          <div className="preview-icon bg-primary">
-                            <i className="mdi mdi-file-document"></i>
-                          </div>
-                        </div>
-                        <div className="preview-item-content d-sm-flex flex-grow">
-                          <div className="flex-grow">
-                            <h6 className="preview-subject">Admin dashboard design</h6>
-                            <p className="text-muted mb-0">Broadcast web app mockup</p>
-                          </div>
-                          <div className="mr-auto text-sm-right pt-2 pt-sm-0">
-                            <p className="text-muted">15 minutes ago</p>
-                            <p className="text-muted mb-0">30 tasks, 5 issues </p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="preview-item border-bottom">
-                        <div className="preview-thumbnail">
-                          <div className="preview-icon bg-success">
-                            <i className="mdi mdi-cloud-download"></i>
-                          </div>
-                        </div>
-                        <div className="preview-item-content d-sm-flex flex-grow">
-                          <div className="flex-grow">
-                            <h6 className="preview-subject">Wordpress Development</h6>
-                            <p className="text-muted mb-0">Upload new design</p>
-                          </div>
-                          <div className="mr-auto text-sm-right pt-2 pt-sm-0">
-                            <p className="text-muted">1 hour ago</p>
-                            <p className="text-muted mb-0">23 tasks, 5 issues </p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="preview-item border-bottom">
-                        <div className="preview-thumbnail">
-                          <div className="preview-icon bg-info">
-                            <i className="mdi mdi-clock"></i>
-                          </div>
-                        </div>
-                        <div className="preview-item-content d-sm-flex flex-grow">
-                          <div className="flex-grow">
-                            <h6 className="preview-subject">Project meeting</h6>
-                            <p className="text-muted mb-0">New project discussion</p>
-                          </div>
-                          <div className="mr-auto text-sm-right pt-2 pt-sm-0">
-                            <p className="text-muted">35 minutes ago</p>
-                            <p className="text-muted mb-0">15 tasks, 2 issues</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="preview-item border-bottom">
-                        <div className="preview-thumbnail">
-                          <div className="preview-icon bg-danger">
-                            <i className="mdi mdi-email-open"></i>
-                          </div>
-                        </div>
-                        <div className="preview-item-content d-sm-flex flex-grow">
-                          <div className="flex-grow">
-                            <h6 className="preview-subject">Broadcast Mail</h6>
-                            <p className="text-muted mb-0">Sent release details to team</p>
-                          </div>
-                          <div className="mr-auto text-sm-right pt-2 pt-sm-0">
-                            <p className="text-muted">55 minutes ago</p>
-                            <p className="text-muted mb-0">35 tasks, 7 issues </p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="preview-item">
-                        <div className="preview-thumbnail">
-                          <div className="preview-icon bg-warning">
-                            <i className="mdi mdi-chart-pie"></i>
-                          </div>
-                        </div>
-                        <div className="preview-item-content d-sm-flex flex-grow">
-                          <div className="flex-grow">
-                            <h6 className="preview-subject">UI Design</h6>
-                            <p className="text-muted mb-0">New application planning</p>
-                          </div>
-                          <div className="mr-auto text-sm-right pt-2 pt-sm-0">
-                            <p className="text-muted">50 minutes ago</p>
-                            <p className="text-muted mb-0">27 tasks, 4 issues </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-4 grid-margin">
-            <div className="card">
-              <div className="card-body">
-                <h5>Revenue</h5>
-                <div className="row">
-                  <div className="col-8 col-sm-12 col-xl-8 my-auto">
-                    <div className="d-flex d-sm-block d-md-flex align-items-center">
-                      <h2 className="mb-0">$32123</h2>
-                      <p className="text-success ml-2 mb-0 font-weight-medium">+3.5%</p>
-                    </div>
-                    <h6 className="text-muted font-weight-normal">11.38% Since last month</h6>
-                  </div>
-                  <div className="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
-                    <i className="icon-lg mdi mdi-codepen text-primary ml-auto"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-4 grid-margin">
-            <div className="card">
-              <div className="card-body">
-                <h5>Sales</h5>
-                <div className="row">
-                  <div className="col-8 col-sm-12 col-xl-8 my-auto">
-                    <div className="d-flex d-sm-block d-md-flex align-items-center">
-                      <h2 className="mb-0">$45850</h2>
-                      <p className="text-success ml-2 mb-0 font-weight-medium">+8.3%</p>
-                    </div>
-                    <h6 className="text-muted font-weight-normal"> 9.61% Since last month</h6>
-                  </div>
-                  <div className="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
-                    <i className="icon-lg mdi mdi-wallet-travel text-danger ml-auto"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-4 grid-margin">
-            <div className="card">
-              <div className="card-body">
-                <h5>Purchase</h5>
-                <div className="row">
-                  <div className="col-8 col-sm-12 col-xl-8 my-auto">
-                    <div className="d-flex d-sm-block d-md-flex align-items-center">
-                      <h2 className="mb-0">$2039</h2>
-                      <p className="text-danger ml-2 mb-0 font-weight-medium">-2.1% </p>
-                    </div>
-                    <h6 className="text-muted font-weight-normal">2.27% Since last month</h6>
-                  </div>
-                  <div className="col-4 col-sm-12 col-xl-4 text-center text-xl-right">
-                    <i className="icon-lg mdi mdi-monitor text-success ml-auto"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row ">
-          <div className="col-12 grid-margin">
-            <div className="card">
-              <div className="card-body">
-                <h4 className="card-title">Order Status</h4>
+                <h4 className="card-title">Leaderboard</h4>
                 <div className="table-responsive">
                   <table className="table">
                     <thead>
@@ -472,13 +289,8 @@ options = {
                             </label>
                           </div>
                         </th>
-                        <th> Client Name </th>
-                        <th> Order No </th>
-                        <th> Product Cost </th>
-                        <th> Project </th>
-                        <th> Payment Mode </th>
-                        <th> Start Date </th>
-                        <th> Payment Status </th>
+                        <th> Username </th>
+                        <th> Amount Invested </th>
                       </tr>
                     </thead>
                     <tbody>
@@ -498,13 +310,7 @@ options = {
                           </div>
                         </td>
                         <td> 02312 </td>
-                        <td> $14,500 </td>
-                        <td> Dashboard </td>
-                        <td> Credit card </td>
-                        <td> 04 Dec 2019 </td>
-                        <td>
-                          <div className="badge badge-outline-success">Approved</div>
-                        </td>
+                        
                       </tr>
                       <tr>
                         <td>
@@ -608,7 +414,11 @@ options = {
               </div>
             </div>
           </div>
-        </div>
+       
+
+        {/*End Leaderboard*/}
+
+        
         <div className="row">
           <div className="col-md-6 col-xl-4 grid-margin stretch-card">
             <div className="card">
