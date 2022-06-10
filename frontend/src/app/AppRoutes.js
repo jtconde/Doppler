7b1e5d2219675287ab1b57a344aea70548936554
jtconde <juliantcinde@gmail.com>
2022-06-10 08:21:27 -0400

    Update-0-4

--- frontend/src/app/AppRoutes.js
+++ frontend/src/app/AppRoutes.js
@@ -5,11 +5,13 @@ import Spinner from '../app/shared/Spinner';
 
 const Dashboard = lazy(() => import('./dashboard/Dashboard'));
 
-const Buttons = lazy(() => import('./basic-ui/Buttons'));
-const Dropdowns = lazy(() => import('./basic-ui/Dropdowns'));
+
+const Notifications = lazy(() => import('./basic-ui/Notifications'));
 const Typography = lazy(() => import('./basic-ui/Typography'));
 const Coin = lazy(() => import('./basic-ui/Coin'));
 const Trending = lazy(() => import('./basic-ui/Trending'));
+const Profile = lazy(() => import('./basic-ui/Profile'));
+const Follows = lazy(() => import('./basic-ui/Follows'));
 
 
 const BasicElements = lazy(() => import('./form-elements/BasicElements'));
@@ -34,11 +36,14 @@ class AppRoutes extends Component {
         <Switch>
           <Route exact path="/dashboard" component={ Dashboard } />
 
-          <Route path="/basic-ui/buttons" component={ Buttons } />
-          <Route path="/basic-ui/dropdowns" component={ Dropdowns } />
+
+          <Route path="/basic-ui/profile" component={ Profile } />
+
+          <Route path="/basic-ui/notifications" component={ Notifications } />
           <Route path="/basic-ui/typography" component={ Typography } />
           <Route path="/basic-ui/coin" component={ Coin } />
           <Route path="/basic-ui/trending" component={ Trending } />
+          <Route path="/basic-ui/follows" component={ Follows } />
 
           <Route path="/form-Elements/basic-elements" component={ BasicElements } />
 
